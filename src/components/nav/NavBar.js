db7d9d7828ce1286f8b2a3efb8ecc7ce9d1acfdb
Austin Graham <austingraham731@gmail.com>
2018-10-21 14:57:35 -0500

    navbar with search

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import './NavBar.css';
 
 class NavBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        window.location.assign('/');
+    }
+
     render() {
         return(
             <div style={ styles.barStyle }>
-                <h1>NavBar!</h1>
+                <div style={ styles.labelStyle }>
+                    <h1 onClick={this.handleClick}>ProductDiscover</h1>
+                </div>
             </div>
         )
     }
@@ -12,7 +25,18 @@ class NavBar extends Component {
 
 const styles = {
     barStyle: {
-
+        background: 'black',
+        color: '#06C4EE',
+        height: '75px',
+        top: 0,
+        left: 0,
+        position: 'fixed',
+        width: '100%',
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+    },
+    labelStyle: {
+        paddingLeft: '50px',
+        fontFamily: 'Walkway'
     }
 }
 
